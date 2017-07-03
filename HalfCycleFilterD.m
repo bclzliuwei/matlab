6757a3e0c0fc94adf5c146e2342023b8df885155
bclzliuwei <bclzliuwei@gmail.com>
2017-07-03 11:27:32 +0100

    Update HalfCycleFilterD.m

--- HalfCycleFilterD.m
+++ HalfCycleFilterD.m
@@ -2,6 +2,10 @@ function [ HCosine, HSine ] = HalfCycleFilterD( FilterT, SignalT )
 %HalfCycleFilter 此处显示有关此函数的摘要
 %   此处显示详细说明
 
+% 0.5 is very special 
+% and if 0.5 is applied, HCosine and HSine are orthogonal.
+Offset = 0.5;
+
 if FilterT == 0
     error('FilterT should not be zero!');
 end
@@ -14,11 +18,11 @@ HCosine = 0;
 HSine = 0;
 for Index = 1 : FilterT
    HCosine = HCosine ...
-       + cos(2 * pi * Index / (FilterT * 2)) ...
+       + cos(2 * pi * (Index - Offset) / (FilterT * 2)) ...
        * exp(-2 * pi / SignalT * Index * 1i);
    
    HSine = HSine ...
-       + sin(2 * pi * Index / (FilterT * 2)) ...
+       + sin(2 * pi * (Index - Offset) / (FilterT * 2)) ...
        * exp(-2 * pi / SignalT * Index * 1i);
 end
 
