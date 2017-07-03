020d73003a3c09ee305dd946173d9cc67f21367e
bclzliuwei <bclzliuwei@gmail.com>
2017-07-03 11:42:09 +0100

    Update FullCycleFilterD.m

--- FullCycleFilterD.m
+++ FullCycleFilterD.m
@@ -1,6 +1,9 @@
 function [ HCosine, HSine ] = FullCycleFilterD( FilterT, SignalT )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+
+Offset = 0.5;
+
 if FilterT == 0
     error('FilterT should not be zero!');
 end
@@ -13,11 +16,11 @@ HCosine = 0;
 HSine = 0;
 for Index = 1 : FilterT
    HCosine = HCosine ...
-       + cos(2 * pi * Index / FilterT) ...
+       + cos(2 * pi * (Index - Offset) / FilterT) ...
        * exp(-2 * pi / SignalT * Index * 1i);
    
    HSine = HSine ...
-       + sin(2 * pi * Index / FilterT) ...
+       + sin(2 * pi * (Index - Offset) / FilterT) ...
        * exp(-2 * pi / SignalT * Index * 1i);
 end
 
