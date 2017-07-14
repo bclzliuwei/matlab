6d8584aeb5b396f8e6e8973b8131a46aae68fa65
Wei LIU <bclzliuwei@gmail.com>
2017-07-14 08:35:43 +0100

    time parameter was added.

--- TriangularFIRFilter.m
+++ TriangularFIRFilter.m
@@ -6,7 +6,8 @@ TriangularFIRCoef = [1/32 2/32 3/32 4/32 5/32 6/32 7/32 8/32];
 Result = 0;
 
 for Index = 1 : length(TriangularFIRCoef)
-    Result = Result + TriangularFIRCoef(Index) * exp(-2 * pi * Multiplier * Fs *1i);
+    Result = Result + TriangularFIRCoef(Index) ...
+        * exp(-2 * pi * Multiplier * Fs * Index * 1i);
 end
 
 HResponse = Result;
